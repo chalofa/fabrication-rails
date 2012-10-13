13efdc2fb48a2c587bdc0b7ce0a2c8d1353fd424
Chalo Fernandez <chalofa@gmail.com>
2012-10-13 14:19:56 -0500

    Tentative code, unfortunately not working because uninitialized constant error

--- lib/fabrication-rails.rb
+++ lib/fabrication-rails.rb
@@ -1,6 +1,10 @@
-require 'fabrication-rails/version'
+require 'rails'
+require 'fabrication'
 
-module Fabrication
-  module Rails
+module FabricationRails
+  class Railtie < ::Rails::Railtie
+    # Rails-3.0.1 requires config.app_generators instead of 3.0.0's config.generators
+    generators = config.respond_to?(:app_generators) ? config.app_generators : config.generators
+    generators.fixture_replacement :fabrication
   end
 end
